59d68edbc387b7c3b84bfd0c3b5a4e42da17ad44
Alex Tahk <tahk@alum.mit.edu>
2018-01-19 00:20:37 -0600

    actually fixed python scripts

--- prebuild.py
+++ prebuild.py
@@ -7,9 +7,12 @@ import io
 if __name__ == "__main__":
 
     processes = []
-    for shape in ["Roman", "Italic"]:
-        with io.open('build-instance-'+shape+'.log', mode='wb') as out:
-            p = subprocess.Popen(["makeInstancesUFO", "-d", shape+"Masters/SourceCodePro.designspace"], stdout=out, stderr=out)
-            child_processes.append(p)
+    files = dict()
+    for shape in ["RomanMasters/SourceCodePro.designspace", "ItalicMasters/SourceCodePro-It.designspace"]:
+        files[shape] = io.open('build-instance-'+shape.split("/")[0]+'.log', mode='wt')
+        p = subprocess.Popen(["makeInstancesUFO", "-d", shape],
+                             bufsize=1, stdout=files[shape], stderr=files[shape])
+        processes.append(p)
+
     for p in processes:
         p.wait()
